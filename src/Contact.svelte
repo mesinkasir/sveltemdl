0126d6fa5662f2917c501c55eb3515512ff67746
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2021-10-23 22:46:28 +0700

    Update Contact.svelte
    Fixed contacts

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Footer from "./widget/Footer.svelte";
-	import Cardcontact from "./card/Cardcontact.svelte";
+	import Cardcontact from "./card/CardContact.svelte";
 </script>
 <div class="row p-3">
 	<div class="col-12 col-md-12 p-3">
